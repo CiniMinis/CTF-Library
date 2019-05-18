82057d892ec47553149e6d7eae872ddc64f9d3b4
Jonathan Kalish <CiniMinis@users.noreply.github.com>
2019-05-18 17:23:50 +0300

    Update test_server.py

--- test_server.py
+++ test_server.py
@@ -1,12 +1,14 @@
 import ctf_lib
 
+
 def win1(client_sock):
 	return True
 
-challenge1 = ctf_lib.Challenge(4416, "flag1", win1)
+
+challenge1 = ctf_lib.Challenge("Sample Challenge", 150, 4416, "flag1", win1)
 challenge1.start_challenge()
 
-while "stop" != raw_input():
+while "stop" != raw_input().lower():
 	pass
 
 challenge1.stop_challenge()
